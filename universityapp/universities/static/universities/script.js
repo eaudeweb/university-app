dee77546eb07e1bf8b0dd80203d4ef9974be40ca
DvdInt <137161224+DvdInt@users.noreply.github.com>
2023-06-23 12:43:20 +0300

    Add files via upload

---
+++ universityapp/universities/static/universities/script.js
@@ -0,0 +1,11 @@
+function Dark() {
+    document.body.style.backgroundColor = 'rgb(41, 41, 61)';
+    document.body.style.color = "white"
+    document.querySelector('.title').style.color = 'black';
+}
+
+function Light() {
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = "black"
+    document.querySelector('.title').style.color = 'black';
+}
